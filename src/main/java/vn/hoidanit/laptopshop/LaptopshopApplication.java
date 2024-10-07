975c99b3cf1db6831702e09dcbe6086c9a09cc1a
thanhht05 <huuthanhht05@gmail.com>
2024-10-07 21:25:46 +0700

    bai3

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -3,7 +3,10 @@ package vn.hoidanit.laptopshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
+
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
+
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
