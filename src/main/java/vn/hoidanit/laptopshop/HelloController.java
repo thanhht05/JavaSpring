e854852cd887026b04f12ed8a442790f4fe1b800
thanhht05 <huuthanhht05@gmail.com>
2024-10-07 13:15:57 +0700

    fixerror

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World thanh";
+        return "Hello World  đf";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin";
     }
 }
